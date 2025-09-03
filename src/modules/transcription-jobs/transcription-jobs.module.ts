2a9dfcb20f7627ed4323e7d2915727f0d1e07cd7
jomasuleymen <joma.suleymen@gmail.com>
2025-09-03 20:15:15 +0500

    create abstract DAO amd memory implementation for transcription jobs

--- src/modules/transcription-jobs/transcription-jobs.module.ts
+++ src/modules/transcription-jobs/transcription-jobs.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TranscriptionJobsMemoryDao } from './dao/transcription-jobs-memory.dao';
+import { TRANSCRIPTION_JOBS_DAO } from './dao/transcription-jobs.dao.interface';
 import { TranscriptionJobsResolver } from './transcription-jobs.resolver';
 import { TranscriptionJobsService } from './transcription-jobs.service';
 
 @Module({
-  providers: [TranscriptionJobsResolver, TranscriptionJobsService],
+  providers: [
+    TranscriptionJobsResolver,
+    TranscriptionJobsService,
+    {
+      provide: TRANSCRIPTION_JOBS_DAO,
+      useClass: TranscriptionJobsMemoryDao,
+    },
+  ],
   exports: [TranscriptionJobsService],
 })
 export class TranscriptionJobsModule {}
--- src/modules/transcription-jobs/transcription-jobs.service.ts
+++ src/modules/transcription-jobs/transcription-jobs.service.ts
@@ -2,18 +2,24 @@ import { PutObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { S3_BUCKET_NAMES } from '@core/s3/s3.constants';
 import { S3Service } from '@core/s3/s3.service';
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { SystemUtils } from '@shared/utils/system.utils';
 import { v4 as uuidv4 } from 'uuid';
+import {
+  TRANSCRIPTION_JOBS_DAO,
+  type TranscriptionJobsDao,
+} from './dao/transcription-jobs.dao.interface';
 import { CreateTranscriptionJobResponse } from './dto/create-transcription-job-response';
 import { CreateTranscriptionJobInput } from './dto/create-transcription-job.input';
 import { TranscriptionJob, TranscriptionJobStatus } from './entities/transcription-job.entity';
 
 @Injectable()
 export class TranscriptionJobsService {
-  private transcriptions: TranscriptionJob[] = [];
-
-  constructor(private readonly s3Service: S3Service) {}
+  constructor(
+    private readonly s3Service: S3Service,
+    @Inject(TRANSCRIPTION_JOBS_DAO)
+    private readonly transcriptionJobsDao: TranscriptionJobsDao,
+  ) {}
 
   async create(createInput: CreateTranscriptionJobInput): Promise<CreateTranscriptionJobResponse> {
     const transcriptionJob: TranscriptionJob = {
@@ -28,7 +34,7 @@ export class TranscriptionJobsService {
       createInput.contentType,
     );
 
-    this.transcriptions.push(transcriptionJob);
+    await this.transcriptionJobsDao.create(transcriptionJob);
 
     return {
       job: transcriptionJob,
@@ -37,11 +43,11 @@ export class TranscriptionJobsService {
   }
 
   async findAll(): Promise<TranscriptionJob[]> {
-    return this.transcriptions.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
+    return await this.transcriptionJobsDao.findAll();
   }
 
   async findOne(id: string): Promise<TranscriptionJob> {
-    const transcription = this.transcriptions.find((t) => t.id === id);
+    const transcription = await this.transcriptionJobsDao.findById(id);
     if (!transcription) {
       throw new NotFoundException(`Transcription with ID ${id} not found`);
     }
@@ -52,10 +58,6 @@ export class TranscriptionJobsService {
   async confirmJobFileUpload(id: string): Promise<TranscriptionJob> {
     const transcriptionJob = await this.findOne(id);
 
-    if (!transcriptionJob) {
-      throw new NotFoundException(`Transcription job with ID ${id} not found`);
-    }
-
     if (transcriptionJob.status !== TranscriptionJobStatus.WAITING) {
       throw new BadRequestException('Transcription job is not waiting');
     }
@@ -65,14 +67,18 @@ export class TranscriptionJobsService {
       transcriptionJob.id,
     );
 
-    transcriptionJob.status = TranscriptionJobStatus.PROCESSING;
-    transcriptionJob.s3Url = s3Url;
+    await this.transcriptionJobsDao.update(transcriptionJob.id, {
+      status: TranscriptionJobStatus.PROCESSING,
+      s3Url,
+    });
 
     this.transcribeAudio(s3Url)
       .then(async (transcriptionText) => {
-        transcriptionJob.status = TranscriptionJobStatus.COMPLETED;
-        transcriptionJob.transcriptionText = transcriptionText;
-        transcriptionJob.completedAt = new Date();
+        await this.transcriptionJobsDao.update(transcriptionJob.id, {
+          status: TranscriptionJobStatus.COMPLETED,
+          transcriptionText,
+          completedAt: new Date(),
+        });
       })
       .catch(() => {});
 
