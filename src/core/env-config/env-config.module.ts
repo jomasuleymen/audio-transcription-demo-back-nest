18607839f56b9c3d226fe0f935d59666dcd48972
jomasuleymen <joma.suleymen@gmail.com>
2025-09-03 18:48:08 +0500

    Refactor main bootstrap logic

--- src/core/env-config/env-config.module.ts
+++ src/core/env-config/env-config.module.ts
@@ -1,10 +1,8 @@
-// env-config.module.ts
-import { Global, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { EnvConfigService } from './env-config.service';
 import { validate } from './env-config.validation';
 
-@Global()
 @Module({
   imports: [
     ConfigModule.forRoot({
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import { EnvConfigService } from '@core/env-config/env-config.service';
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+
+  const logger = app.get<Logger>(Logger);
+
+  const envConfigService = app.get(EnvConfigService);
+  const port = envConfigService.getPort();
+
+  await app.listen(port, () => {
+    logger.log(`Server is running on port ${port}`);
+  });
 }
-bootstrap();
+
+bootstrap().catch((error) => {
+  console.error('Application failed to start:', error);
+  process.exit(1);
+});
